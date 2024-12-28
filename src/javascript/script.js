42267e6fc16621dfe5ecd7bf74390be23580ffcd
marcelobenjamin <marcelobenjaminrocha100@gmail.com>
2024-12-28 17:53:27 -0300

    fix: fake hamburger in work.css

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,21 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu ul');
-  
+  console.log("linha 4")
+
+  // função do hamburger no celular
   hamburger.addEventListener('click', function() {
     menu.classList.toggle('active');
     hamburger.classList.toggle('active');
   });
 
   // JavaScript para destacar a seção atual com a cor do link
-  const secoes = document.querySelectorAll('section');
-  const menuLinks = document.querySelectorAll('.menu a');
-
   function mudarAtivo() {
+    const secoes = document.querySelectorAll('section');
+    const menuLinks = document.querySelectorAll('.menu a');
+
     let index = secoes.length;
+    console.log("linha 15")
 
     while(--index && window.scrollY + 50 < secoes[index].offsetTop) {}
 
+    
     menuLinks.forEach((link) => link.classList.remove('secao-ativa'));
     if (menuLinks[index]) menuLinks[index].classList.add('secao-ativa');
   }
@@ -38,8 +42,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
   const prevBtn = document.querySelector('.carousel-btn.prev');
@@ -87,70 +89,6 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   window.addEventListener('resize', updateCarousel);
-
-  updateCarousel();
-  handleButtons();
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  const prevBtn = document.querySelector('#beneficios .carousel-btn.prev');
-  const nextBtn = document.querySelector('#beneficios .carousel-btn.next');
-  const cards = document.querySelector('#beneficios .cards');
-  let currentIndex = 0;
-  const totalCards = 6;
-  let cardWidth = cards.children[1].offsetWidth; 
-  let offsetIncrement = 65;
-
-  function adjustOffsetIncrement() {
-    if (window.innerWidth > 768) {
-      offsetIncrement = 25; // ou qualquer valor que você deseja para janelas maiores
-    } else {
-      offsetIncrement = 65; // valor padrão para janelas menores
-    }
-  }
-
-  adjustOffsetIncrement();
-
-  prevBtn.addEventListener('click', function() {
-    if (currentIndex > 0) {
-      currentIndex--;
-      updateCarousel();
-      handleButtons();
-    }
-  });
-
-  nextBtn.addEventListener('click', function() {
-    if (currentIndex < totalCards - 1) { 
-      currentIndex++;
-      updateCarousel();
-      handleButtons();
-    }
-  });
-
-  function updateCarousel() {
-    cardWidth = cards.children[1].offsetWidth;
-    const newTransform = -currentIndex * cardWidth - (currentIndex * offsetIncrement);
-    cards.style.transform = `translateX(${newTransform}px)`;
-  }
-
-  function handleButtons() {
-    if (currentIndex === 0) {
-      prevBtn.style.display = 'none';
-    } else {
-      prevBtn.style.display = 'block';
-    }
-
-    if (currentIndex >= totalCards - 1) {
-      nextBtn.style.display = 'none';
-    } else {
-      nextBtn.style.display = 'block';
-    }
-  }
-
-  window.addEventListener('resize', function() {
-    adjustOffsetIncrement();
-    updateCarousel();
-  });
 
   updateCarousel();
   handleButtons();
