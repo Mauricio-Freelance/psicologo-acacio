1fad2bfff206fe2be8f3a3e90db95d91bc39f3ec
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-23 08:31:46 -0300

    test: remove bug files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: './', // Adicione esta linha
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
